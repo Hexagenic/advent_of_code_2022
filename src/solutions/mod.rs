3012ee9148c3f1cc648b507dd56d7861493f422f
Mattias Hermansson <mattias@hermansson.dev>
2022-12-01 18:52:42 +0100

    refactor: replace switch with slice

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -8,12 +8,15 @@ pub enum Solution {
 
 pub type PuzzleSolution = Option<fn(&str) -> Solution>;
 
+const DAYS: [(PuzzleSolution, PuzzleSolution); 1] = [(Some(day01::part_a), Some(day01::part_b))];
+
 #[must_use]
 pub fn get_solution(day: u8) -> (PuzzleSolution, PuzzleSolution) {
-    match day {
-        1 => (Some(day01::part_a), Some(day01::part_b)),
-        _ => (None, None),
+    if day > 0 && day <= MAX_SOLVED_DAY {
+        DAYS[day as usize - 1]
+    } else {
+        (None, None)
     }
 }
 
-pub const MAX_SOLVED_DAY: u8 = 1;
+pub const MAX_SOLVED_DAY: u8 = DAYS.len() as u8;
