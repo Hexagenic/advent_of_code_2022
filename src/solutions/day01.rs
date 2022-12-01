622a5e240c5dc42bf2129e4ca8eaa0d28ade688b
Mattias Hermansson <mattias@hermansson.dev>
2022-12-01 18:25:47 +0100

    feat: Solve day 1

--- src/solutions/day01.rs
+++ src/solutions/day01.rs
@@ -1,11 +1,50 @@
 use crate::solutions::Solution;
 
 pub fn part_a(file: &str) -> Solution {
-    Solution::Integer(file.len() as i64)
+    let nums = string_to_numbers(file);
+
+    let mut highest: i64 = 0;
+    let mut current: i64 = 0;
+
+    for num in nums {
+        if let Some(n) = num {
+            current += n;
+        } else {
+            if current > highest {
+                highest = current;
+            }
+            current = 0;
+        }
+    }
+
+    Solution::Integer(highest)
 }
 
 pub fn part_b(file: &str) -> Solution {
-    Solution::Integer(file.len() as i64)
+    let nums = string_to_numbers(file);
+
+    let mut elfs = vec![];
+
+    let mut current: i64 = 0;
+
+    for num in nums {
+        if let Some(n) = num {
+            current += n;
+        } else {
+            elfs.push(current);
+            current = 0;
+        }
+    }
+
+    elfs.push(current);
+
+    elfs.sort();
+
+    Solution::Integer(elfs.iter().rev().take(3).sum())
+}
+
+fn string_to_numbers(file: &str) -> Vec<Option<i64>> {
+    file.lines().map(|l| l.parse().ok()).collect()
 }
 
 #[cfg(test)]
@@ -13,7 +52,22 @@ mod tests {
     use super::*;
 
     #[test]
-    fn tmp_test() {
-        assert_eq!(Solution::Integer(3), part_a("aaa"));
+    fn convert_string() {
+        assert_eq!(
+            vec![Some(1), Some(2), Some(3), None, Some(1)],
+            string_to_numbers("1\n2\n3\n\n1")
+        );
+    }
+
+    #[test]
+    fn test_a() {
+        let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+        assert_eq!(Solution::Integer(24000), part_a(input));
+    }
+
+    #[test]
+    fn test_b() {
+        let input = "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000";
+        assert_eq!(Solution::Integer(45000), part_b(input));
     }
 }
